9a57f52c40f2adb8b488757fd2dc129d989a65b7
zzy131250 <zzy131250@gmail.com>
2015-04-02 19:28:22 +0800

    4.2dataservice

--- src/main/java/dataservice/getdatadataservice/GetTeamdataDataService.java
+++ src/main/java/dataservice/getdatadataservice/GetTeamdataDataService.java
@@ -10,15 +10,15 @@ import po.TeaminfoPO;
 
 public interface GetTeamdataDataService extends Remote {
 
-	public TeamPO getTeamdata(String teamName)throws RemoteException;
+	public TeamPO getTeamdata(String season,String teamName)throws RemoteException;
 	
 	public TeaminfoPO getTeaminfo(String teamName)throws RemoteException;
 	
-	public ArrayList<String> getTeamPlayer(String teamName)throws RemoteException;
+	public ArrayList<String> getTeamPlayer(String season,String teamName)throws RemoteException;
 	
-	public ArrayList<TeamPO> getAllTeamdata(String key,String order)throws RemoteException;
+	public ArrayList<TeamPO> getAllTeamdata(String season,String key,String order)throws RemoteException;
 	
-	public ArrayList<TeamPO> getSomeTeamdata(String condition,String key,String order)throws RemoteException;
+	public ArrayList<TeamPO> getSomeTeamdata(String season,String condition,String key,String order)throws RemoteException;
 	
 	public ArrayList<TeamPO> getByEfficiency(ArrayList<TeamPO> po,String key,String order)throws RemoteException;
 	
